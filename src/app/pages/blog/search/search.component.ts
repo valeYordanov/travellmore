9b59442c19d8ceb01c02f352b9be9eff493d67b0
valeYordanov <valio052@gmail.com>
2024-03-16 10:31:58 +0200

    Merge branch 'main' of https://github.com/valeYordanov/travellmore

---
+++ src/app/pages/blog/search/search.component.ts
@@ -0,0 +1,12 @@
+import{component} from@ang
+
+@Component({
+  selector: 'app-search',
+  standalone: true,
+  imports: [],
+  templateUrl: './search.component.html',
+  styleUrl: './search.component.css'
+})
+export class SearchComponent {
+
+}
